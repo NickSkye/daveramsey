d9e1be6d359a347188f4a3c9b99cd08532e5958c
NickSkye <nickskye7@gmail.com>
2019-03-05 20:42:16 -0800

    added stuff

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,50 @@
+
+var startPanel = 1;
+
+$(document).ready(function(){
+
+    window.panelWidth = $('.sp').width();
+
+    $('.panel-container .panel').each(function(index){
+
+        $(this).css({'width':window.panelWidth+'px','left':(index*window.panelWidth)+'px'});
+
+        $('.sp .panels').css('width',(index+1)*window.panelWidth+'px');
+
+    });
+    /*Add click event to items */
+    $('.sp .items span').each(function(){
+        $(this).on('click', function(){
+            changePanels( $(this).index() );
+        });
+    });
+
+    /* Trigger the first panel */
+    $('.sp .items span:nth-child('+window.startPanel+')').trigger('click');
+
+});
+
+
+function changePanels(newIndex){
+
+    var newPanelPosition = ( window.panelWidth * newIndex ) * -1;
+    var newPanelHeight = $('.sp .panel:nth-child('+(newIndex+1)+')').find('.panel-content').height() + 0;
+
+
+
+    $('.sp .items span').removeClass('selected');
+    $('.sp .items span:nth-child('+(newIndex+1)+')').addClass('selected');
+
+    $('.sp .panels').animate({left:newPanelPosition},1000);
+    $('.sp .panel-container').animate({height:newPanelHeight},1000);
+
+    $.get("/baby-steps.json").done( function(data){
+        $.each(data.friends, function(index, value) {
+
+                console.log(value.babyStep);
+
+
+        });
+    });
+
+}
